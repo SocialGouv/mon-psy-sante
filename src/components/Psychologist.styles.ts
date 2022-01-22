fe5bedecce96a97de12ef43dacc7c3559382e4c8
Desoindx <xavier.desoindre@hotmail.fr>
2022-01-22 17:35:08 +0100

    Add website

--- src/components/Psychologist.styles.ts
+++ src/components/Psychologist.styles.ts
@@ -2,6 +2,8 @@ import styled, { css } from "styled-components";
 
 export const PsychologistWrapper = styled.div`
   cursor: pointer;
+  background-color: var(--background-contrast-grey);
+  padding: 24px;
   ${(props) =>
     css`
       ${props.selected
--- src/components/Psychologist.tsx
+++ src/components/Psychologist.tsx
@@ -1,4 +1,3 @@
-import { Card, CardDescription, CardTitle } from "@dataesr/react-dsfr";
 import React from "react";
 
 import { Psychologist as PsychologistType } from "../types/psychologist";
@@ -7,6 +6,15 @@ import { PsychologistWrapper } from "./Psychologist.styles";
 const infos = [
   { label: "Adresse:", value: "address" },
   { label: "Téléphone:", value: "phone" },
+  {
+    label: "Site internet:",
+    value: (psy) =>
+      psy.website ? (
+        <a href={psy.website} rel="noreferrer" target="_blank">
+          {psy.website}
+        </a>
+      ) : null,
+  },
   { label: "Accepte la téléconsultation", value: "teleconsultation" },
   { label: "Travail avec les enfants", value: "withChildren" },
 ];
@@ -21,22 +29,27 @@ const Psychologist = ({
   selected: boolean;
 }) => {
   return (
-    <PsychologistWrapper selected={selected} className="fr-mb-2w">
-      <Card hasArrow={false} anchorAs="span" onClick={onClick}>
-        <CardTitle as="h2">
-          {psychologist.firstName} {psychologist.lastName}
-        </CardTitle>
-        <CardDescription as="div">
-          {infos.map((info) => {
-            const value = psychologist[info.value];
-            return value ? (
-              <p key={info.label} className="fr-my-1w">
-                {info.label} {value}
-              </p>
-            ) : null;
-          })}
-        </CardDescription>
-      </Card>
+    <PsychologistWrapper
+      selected={selected}
+      className="fr-mb-2w"
+      onClick={onClick}
+    >
+      <h2>
+        {psychologist.firstName} {psychologist.lastName}
+      </h2>
+      <div>
+        {infos.map((info) => {
+          const value =
+            typeof info.value === "string"
+              ? psychologist[info.value]
+              : info.value(psychologist);
+          return value ? (
+            <p key={info.label} className="fr-my-1w">
+              {info.label} {value}
+            </p>
+          ) : null;
+        })}
+      </div>
     </PsychologistWrapper>
   );
 };
