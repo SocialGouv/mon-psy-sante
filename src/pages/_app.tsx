8e2e46f03a1041ad243029ff5b3fa7d8ba503b9f
carolineBda <caroline.bourdeu@gmail.com>
2022-02-01 14:57:32 +0100

    fix(text): update dispositif name (lint !)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -19,18 +19,18 @@ const MATOMO_SITE_ID = process.env.NEXT_PUBLIC_MATOMO_SITE_ID;
 
 class MyApp extends App {
   componentDidMount() {
-    init({siteId: MATOMO_SITE_ID, url: MATOMO_URL});
+    init({ siteId: MATOMO_SITE_ID, url: MATOMO_URL });
   }
 
   render() {
-    const {Component, pageProps} = this.props;
+    const { Component, pageProps } = this.props;
 
     return (
       <>
         <Head>
-          <link rel="shortcut icon" href="/favicon.ico"/>
+          <link rel="shortcut icon" href="/favicon.ico" />
           <title>MonPsy</title>
-          <meta property="og:title" content="MonPsy"/>
+          <meta property="og:title" content="MonPsy" />
 
           <meta
             name="description"
@@ -41,7 +41,7 @@ class MyApp extends App {
             content="MonPsy s’adresse à toute la population à partir de 3 ans présentant des troubles psychiques d’intensité légère à modérée. Dès le printemps 2022, sur orientation d’un médecin, les patients (enfants, adolescents et adultes) pourront bénéficier de séances assurées par des psychologues volontaires conventionnés avec l’Assurance Maladie."
           />
 
-          <meta property="og:type" content="website"/>
+          <meta property="og:type" content="website" />
           <script
             type="text/javascript"
             src="https://forms.sbc08.com/form.js"
@@ -56,11 +56,11 @@ class MyApp extends App {
           <SkiplinkItem href="#header-navigation">Menu</SkiplinkItem>
           <SkiplinkItem href="#footer">Pied de page</SkiplinkItem>
         </Skiplinks>
-        <Nav/>
+        <Nav />
         <div id="contenu">
           <Component {...pageProps} />
         </div>
-        <Footer/>
+        <Footer />
       </>
     );
   }
