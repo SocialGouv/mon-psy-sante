e2e10b15f2fec4128b7ef263d5c47c40a68077ad
carolineBda <caroline.bourdeu@gmail.com>
2022-03-01 22:47:01 +0100

    Increase timeout

--- jest.config-api.js
+++ jest.config-api.js
@@ -1,7 +1,9 @@
 module.exports = {
-  globalSetup: '<rootDir>jest/api-setup.js',
-  globalTeardown: '<rootDir>jest/api-teardown.js',
-  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
-  testMatch: ['**/*.spec.ts'],
-  moduleDirectories: ['<rootDir>', 'node_modules'],
+  globalSetup: "<rootDir>jest/api-setup.js",
+  globalTeardown: "<rootDir>jest/api-teardown.js",
+  testPathIgnorePatterns: ["<rootDir>/.next/", "<rootDir>/node_modules/"],
+  testMatch: ["**/*.spec.ts"],
+  moduleDirectories: ["<rootDir>", "node_modules"],
 };
+
+jest.setTimeout(30000);
