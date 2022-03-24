6cc38fa89bb918d6c0977e0cb7efd6550b4bbd10
carolineBda <caroline.bourdeu@gmail.com>
2022-03-24 13:19:13 +0100

    clean-up

--- next.config.js
+++ next.config.js
@@ -56,19 +56,4 @@ const moduleExports = {
   },
 };
 
-const sentryWebpackPluginOptions = {
-  // Additional config options for the Sentry Webpack plugin. Keep in mind that
-  // the following options are set automatically, and overriding them is not
-  // recommended:
-  //   release, url, org, project, authToken, configFile, stripPrefix,
-  //   urlPrefix, include, ignore
-  disableClientWebpackPlugin: true,
-  disableServerWebpackPlugin: true,
-  silent: true, // Suppresses all logs
-  // For all available options, see:
-  // https://github.com/getsentry/sentry-webpack-plugin#options.
-};
-
-// Make sure adding Sentry options is the last code to run before exporting, to
-// ensure that your source maps include changes from all other Webpack plugins
-module.exports = withSentryConfig(moduleExports, sentryWebpackPluginOptions);
+module.exports = withSentryConfig(moduleExports, { silent: true });
