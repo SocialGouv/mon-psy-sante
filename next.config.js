412fff97bc22c400da55c531a50c4240508e4cde
carolineBda <caroline.bourdeu@gmail.com>
2022-03-24 10:25:47 +0100

    put back webpack option

--- next.config.js
+++ next.config.js
@@ -43,6 +43,7 @@ module.exports = {
   },
   webpack: (config, { isServer }) => {
     if (!isServer) {
+      config.resolve.alias["@sentry/node"] = "@sentry/browser"
       config.resolve.fallback = {
         fs: false,
         path: false,
