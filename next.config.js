32ec038470b0b8f309e02a3d3b478b0671407a6c
carolineBda <caroline.bourdeu@gmail.com>
2022-03-24 13:13:29 +0100

    fix fix

--- next.config.js
+++ next.config.js
@@ -1,5 +1,28 @@
 const { withSentryConfig } = require("@sentry/nextjs");
+const csp = {
+  "default-src": ["'self'"],
+  "connect-src": [
+    "'self'",
+    "https://*.gouv.fr",
+    "https://services.sarbacane.com",
+  ],
+  "font-src": ["'self'", "data:"],
+  "img-src": [
+    "'self'",
+    "data:",
+    "https://forms.sbc08.com",
+    "https://*.tile.openstreetmap.org/",
+  ],
+  "prefetch-src": ["'self'", "https://*.gouv.fr"],
+  "script-src": ["'self'", "https://*.gouv.fr", "https://*.sbc08.com"],
+  "frame-src": ["'self'", "https://*.gouv.fr"],
+  "style-src": ["'self'", "'unsafe-inline'"],
+};
 
+// In dev we allow 'unsafe-eval', so HMR doesn't trigger the CSP
+if (process.env.NODE_ENV !== "production") {
+  csp["script-src"].push("'unsafe-eval'");
+}
 const moduleExports = {
   sentry: {
     disableClientWebpackPlugin: true,
