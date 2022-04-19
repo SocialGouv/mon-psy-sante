c8a7258793a4fa34f66edfe3cbbf833187cb7174
carolineBda <caroline.bourdeu@gmail.com>
2022-04-19 21:23:44 +0200

    lint

--- scripts/stats-departements.ts
+++ scripts/stats-departements.ts
@@ -1,16 +1,16 @@
 import fs from "fs";
 import _ from "lodash";
 
-import {requestPsychologistsState} from "../src/services/demarchesSimplifiees/buildRequest";
-import {getAllPsychologistList} from "../src/services/demarchesSimplifiees/import";
-import {DEPARTMENTS} from "../src/types/enums/department";
+import { requestPsychologistsState } from "../src/services/demarchesSimplifiees/buildRequest";
+import { getAllPsychologistList } from "../src/services/demarchesSimplifiees/import";
+import { DEPARTMENTS } from "../src/types/enums/department";
 
 const file = fs.createWriteStream("departments-stats.csv");
 
 const extractDepNumber = (dep) => dep.split(" - ")[0];
 
 const deps = DEPARTMENTS.map((dep) => {
-  return {count: 0, name: dep, department: extractDepNumber(dep)};
+  return { count: 0, name: dep, department: extractDepNumber(dep) };
 });
 
 function log(text, count) {
@@ -43,14 +43,14 @@ log("Dep", "total");
 
   const psychologists = result.psychologists;
   console.log(psychologists[0]);
-  const sansSuite = _.filter(psychologists, {state: "sans_suite"});
-  const refuse = _.filter(psychologists, {state: "refuse"});
+  const sansSuite = _.filter(psychologists, { state: "sans_suite" });
+  const refuse = _.filter(psychologists, { state: "refuse" });
 
   const valids = _.reject(
-    _.reject(_.reject(psychologists, {state: "refuse"}), {
+    _.reject(_.reject(psychologists, { state: "refuse" }), {
       state: "sans_suite",
     }),
-    {archived: true}
+    { archived: true }
   );
 
   valids.forEach((psy) => {
@@ -61,10 +61,10 @@ log("Dep", "total");
     dep.count++;
   });
 
-  const enInstruction = _.filter(valids, {state: "en_instruction"});
-  const enConstruction = _.filter(valids, {state: "en_construction"});
-  const accepte = _.filter(valids, {state: "accepte"});
-  const noDossier = _.filter(deps, {count: 0});
+  const enInstruction = _.filter(valids, { state: "en_instruction" });
+  const enConstruction = _.filter(valids, { state: "en_construction" });
+  const accepte = _.filter(valids, { state: "accepte" });
+  const noDossier = _.filter(deps, { count: 0 });
 
   displayList(deps);
 
