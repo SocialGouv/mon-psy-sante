ab15455c4e42d341d4cc5d780c51a6ab02e915a8
carolineBda <caroline.bourdeu@gmail.com>
2022-05-12 10:43:09 +0200

    try to debug loggin

--- src/services/middleware.ts
+++ src/services/middleware.ts
@@ -1,5 +1,12 @@
 export const authorize = (token): boolean => {
   if (!token) return false;
+  console.log("verif middleware");
   const roles = token.roles || [];
+  console.log(
+    "roles",
+    roles,
+    roles.includes("admin") || roles.includes("super-admin")
+  );
+
   return roles.includes("admin") || roles.includes("super-admin");
 };
