e160b24e0f8932c60a986ee1f4f95a679e2115ad
Raphaël Huchet <raph@selego.co>
2022-06-13 16:01:42 +0200

    chore: up

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,12 @@
-module.exports = {
+const nextJest = require('next/jest');
+
+const createJestConfig = nextJest();
+
+module.exports = createJestConfig({
   testMatch: ["**/*.spec.ts"],
   testPathIgnorePatterns: ["<rootDir>/src/__tests__/api"],
   moduleDirectories: ["<rootDir>", "node_modules"],
   testTimeout: 30000
-};
+});
+
+
