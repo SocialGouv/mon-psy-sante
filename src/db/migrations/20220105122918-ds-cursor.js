c6107f0c34d8c06eb9bb82e40beb8e2e4511630b
Raphaël Huchet <raph@selego.co>
2022-07-11 18:27:57 +0200

    refactor: remove obsolete dscursor

--- src/db/migrations/20220105122918-ds-cursor.js
+++ src/db/migrations/20220105122918-ds-cursor.js
@@ -1,35 +1,9 @@
 module.exports = {
-  up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable("ds_cursor", {
-      id: {
-        primaryKey: true,
-        type: Sequelize.INTEGER,
-      },
-      cursor: {
-        allowNull: true,
-        type: Sequelize.STRING,
-      },
-      created_at: {
-        allowNull: false,
-        type: Sequelize.DATE,
-      },
-      updated_at: {
-        allowNull: false,
-        type: Sequelize.DATE,
-      },
-    });
-
-    await queryInterface.bulkInsert("ds_cursor", [
-      {
-        id: 1,
-        cursor: undefined,
-        created_at: new Date(),
-        updated_at: new Date(),
-      },
-    ]);
+  up: async () => {
+    // OBSOLETE
   },
 
-  down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable("ds_cursor");
+  down: async () => {
+    // OBSOLETE
   },
 };
