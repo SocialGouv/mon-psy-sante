8f2bcdd5b741b1653aab1737bc9baef37a2943f8
Raphaël Huchet <raph@selego.co>
2022-09-13 13:58:28 +0200

    fix: lint

--- src/pages/medecins.tsx
+++ src/pages/medecins.tsx
@@ -1,10 +1,8 @@
-import { useRouter } from "next/router";
 import React from "react";
 
 import HeadTag from "../components/HeadTag";
 
 const Page = () => {
-  const router = useRouter();
   return (
     <>
       <HeadTag
