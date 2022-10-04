d287a3a267609e1b1ee57a81fdc50cfbffd41c99
Caroline <4971715+carolineBda@users.noreply.github.com>
2022-10-04 15:30:35 +0200

    feat(contact): modification des labels pour pouvoir être lisible sur toutes les tailles d'écran (RGAA) (#260)

--- src/types/enums/contact.ts
+++ src/types/enums/contact.ts
@@ -1,15 +1,15 @@
 export enum CONTACT_USER_TYPE {
-  PSYCHOLOGIST_INTERESTED = "Psychologue intéressé / en cours de candidature",
-  PSYCHOLOGIST_PARTNER = "Psychologue partenaire MonPsy (conventionné avec l'Assurance Maladie)",
+  PSYCHOLOGIST_INTERESTED = "Psychologue intéressé / candidat",
+  PSYCHOLOGIST_PARTNER = "Psychologue conventionné avec l'Assurance Maladie",
   PUBLIC = "Grand public / Patient / Entourage d'un patient",
   DOCTOR = "Médecin / Professionnel de santé",
   OTHER = "Autre : institutionnels, presse, etc",
 }
 
 export enum CONTACT_REASON {
-  ELIGIBILITY = "Question relative à mon éligibilité",
-  GLOBAL = "Question générale sur le fonctionnement du dispositif",
-  APPLICATION = "Question relative à l'avancement de ma candidature",
+  ELIGIBILITY = "Éligibilité",
+  GLOBAL = "Fonctionnement du dispositif",
+  APPLICATION = "Avancement de ma candidature",
   OTHER = "Autre",
 }
 
