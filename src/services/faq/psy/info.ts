601c90740cd27c6b8bc18dd65d78da3652b4a633
Caroline <4971715+carolineBda@users.noreply.github.com>
2022-10-25 11:06:02 +0200

    Update src/services/faq/psy/info.ts
    Co-authored-by: Raphaël Huchet <rap2hpoutre@users.noreply.github.com>

--- src/services/faq/psy/info.ts
+++ src/services/faq/psy/info.ts
@@ -2,7 +2,7 @@
 export default [
   {
     answer:
-      "<p>Vous pouvez signaler tout changement auprès de votre CPAM de rattachement (avec qui vous avez conventionné) pour mettre à jour :</p>" +
+      "<p>Vous pouvez signaler tout changement auprès de votre CPAM de rattachement (avec qui vous avez conventionné) pour mettre à jour&nbsp;:</p>" +
       "<ul><li>les informations sur le public accompagné, les langues pratiquées ou encore la possibilité de suivi à distance</li>" +
       "<li>vos coordonnées : numéro de téléphone, adresse d'exercice, site internet, email</li></ul>",
     question:
