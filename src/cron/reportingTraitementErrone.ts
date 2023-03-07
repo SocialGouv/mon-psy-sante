ab5cd89fbdee3b2e1bf53551d86a172552a11fd1
carolineBda <caroline.bourdeu@gmail.com>
2023-03-07 10:05:02 +0100

    feat(report): add BM as instructeur to exclude from report

--- src/cron/reportingTraitementErrone.ts
+++ src/cron/reportingTraitementErrone.ts
@@ -18,6 +18,7 @@ const INSTRUCTEURS = {
   "SW5zdHJ1Y3RldXItNjExNTM=": "Beta-Gouv-AG",
   "SW5zdHJ1Y3RldXItNDk3NDI=": "Beta-Gouv-LG",
   "SW5zdHJ1Y3RldXItNzgwMjA=": "Beta-Gouv-CA",
+  "SW5zdHJ1Y3RldXItODM1Mzc=": "CNAM-BM",
 };
 const INSTRUCTEUR_FB = "SW5zdHJ1Y3RldXItNjQzNjI=";
 const DOSSIER_ELIGIBLE = "Q2hhbXAtMTg0NDM5NQ==";
