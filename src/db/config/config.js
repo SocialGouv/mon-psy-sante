ed059664e7f4905b24a3deb187149bb0dc35b91d
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2023-11-22 20:59:13 +0100

    fix: use CNPG (#301)
    * fix: use CNPG
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix s3 secret
    
    ---------
    
    Co-authored-by: Adrien Chauve <adrien@chauve.eu>

--- src/db/config/config.js
+++ src/db/config/config.js
@@ -5,9 +5,21 @@ module.exports = {
   development: {
     dialect: "postgres",
     url: process.env.DATABASE_URL,
+    dialectOptions: {
+      ssl: {
+        require: false,
+        rejectUnauthorized: false,
+      },
+    },
   },
   production: {
     dialect: "postgres",
     url: process.env.DATABASE_URL,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
   },
 };
